ff99c7c3aa796d0f19d70e8c977b0d36377c7e58
Keren <kerensanchezpadron@gmail.com>
2025-08-16 13:19:32 -0400

    [feat]: update ESLint, Prettier config and fix Next.js build warnings

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,7 +1,46 @@
-module.exports = {
-  extends: ['next/core-web-vitals', 'next/typescript', 'plugin:storybook/recommended', 'prettier'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off'
+import nextPlugin from '@next/eslint-plugin-next';
+import tseslint from 'typescript-eslint';
+import unicorn from 'eslint-plugin-unicorn';
+import prettier from 'eslint-plugin-prettier';
+import tailwind from 'eslint-plugin-tailwindcss';
+
+export default [
+  {
+    ignores: ['node_modules/**', '.next/**', 'dist/**', 'build/**'],
   },
-  plugins: ['unicorn']
-};
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': nextPlugin,
+      '@typescript-eslint': tseslint.plugin,
+      unicorn,
+      prettier,
+      tailwind,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    extends: [
+      'plugin:@next/next/core-web-vitals',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:unicorn/recommended',
+      'plugin:tailwindcss/recommended',
+      'prettier',
+    ],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-hooks/exhaustive-deps': 'error',
+      'prefer-const': 'error',
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/filename-case': 'off',
+      'tailwindcss/no-custom-classname': 'off',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+];
